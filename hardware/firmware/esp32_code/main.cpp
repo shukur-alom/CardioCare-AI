a95eb46d0607297fe0768cafad340928fb683063
shukur-alom <shukuralom1234@gmail.com>
2024-12-08 21:40:21 +0600

    Improve heart rate detection logic and handle no finger scenario

--- hardware/firmware/esp32_code/main.cpp
+++ hardware/firmware/esp32_code/main.cpp
@@ -38,36 +38,42 @@ void loop()
 {
     long irValue = particleSensor.getIR();
 
-    if (checkForBeat(irValue) == true)
-    {
-        // We sensed a beat!
-        long delta = millis() - lastBeat;
-        lastBeat = millis();
-
-        beatsPerMinute = 60 / (delta / 1000.0);
-
-        if (beatsPerMinute < 255 && beatsPerMinute > 20)
-        {
-            rates[rateSpot++] = (byte)beatsPerMinute; // Store this reading in the array
-            rateSpot %= RATE_SIZE;                    // Wrap variable
-
-            // Take average of readings
-            beatAvg = 0;
-            for (byte x = 0; x < RATE_SIZE; x++)
-                beatAvg += rates[x];
-            beatAvg /= RATE_SIZE;
-        }
-    }
-
-    Serial.print("IR=");
-    Serial.print(irValue);
-    Serial.print(", BPM=");
-    Serial.print(beatsPerMinute);
-    Serial.print(", Avg BPM=");
-    Serial.print(beatAvg);
-
     if (irValue < 50000)
+    {
+        beatsPerMinute = 0;
+        beatAvg = 0;
         Serial.print(" No finger?");
+    }
+    else
+    {
+        if (checkForBeat(irValue) == true)
+        {
+            // We sensed a beat!
+            long delta = millis() - lastBeat;
+            lastBeat = millis();
+
+            beatsPerMinute = 60 / (delta / 1000.0);
+
+            if (beatsPerMinute < 255 && beatsPerMinute > 20)
+            {
+                rates[rateSpot++] = (byte)beatsPerMinute; // Store this reading in the array
+                rateSpot %= RATE_SIZE;                    // Wrap variable
+
+                // Take average of readings
+                beatAvg = 0;
+                for (byte x = 0; x < RATE_SIZE; x++)
+                    beatAvg += rates[x];
+                beatAvg /= RATE_SIZE;
+            }
+        }
+
+        Serial.print("IR=");
+        Serial.print(irValue);
+        Serial.print(", BPM=");
+        Serial.print(beatsPerMinute);
+        Serial.print(", Avg BPM=");
+        Serial.print(beatAvg);
+    }
 
     Serial.println();
 }
